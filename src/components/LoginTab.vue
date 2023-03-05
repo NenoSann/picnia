c0392d945333427684ed6e6682004c6b3a81cec7
NenoSann <lianghengCN@gmail.com>
2023-03-05 23:50:29 +0800

    add login page

---
+++ src/components/LoginTab.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="login-tab-main">
+        <p class="title">Picnia</p>
+        <input type="mail" class="email" placeholder="邮箱">
+        <input type="password" class="password" placeholder="密码">
+        <button class="login-button">登录</button>
+        <a href="">还没有加入Picnia吗？点击注册！</a>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    isLogin: {
+        type: Boolean,
+        default: true,
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.login-tab-main {
+    max-height: 460px;
+    width: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+    border: 40px solid transparent;
+    box-shadow: 0px 4.30151px 46.2412px rgba(0, 0, 0, 0.06);
+    border-radius: 31.1859px;
+}
+
+.title {
+    font-family: 'Raleway', sans-serif;
+    font-weight: 300;
+    font-size: 2rem;
+    color: black;
+    margin-bottom: 30px;
+    margin-top: 10px;
+    // margin: 40px 0;
+}
+
+input {
+    background-color: #fafafa;
+    border-radius: 6px;
+    width: 100%;
+}
+
+.login-button {
+    width: 100%;
+    background-image: linear-gradient(to right, #00c2ff, #0047ff, );
+    border-radius: 6px;
+    color: white;
+}
+
+a {
+    text-decoration: none;
+
+    &:visited {
+        color: #1DA0FF;
+    }
+}
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        <h1>LoginPage</h1>
+    <div class="login-page-main">
+        <div class="image">
+            <img src="../assets/phone.png">
+        </div>
+        <LoginTab />
     </div>
 </template>
 
 <script setup>
-
+import LoginTab from '../components/LoginTab.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login-page-main {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+img {
+    width: 460px;
+    height: auto;
+}
+
+@media (max-width: 900px) {
+    .image {
+        display: none;
+    }
+}
+</style>
