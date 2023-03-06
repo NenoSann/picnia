7a292f057c899862437b1314a7da4c396db7a6bc
NenoSann <lianghengCN@gmail.com>
2023-03-06 22:33:56 +0800

    add test image && The avatar component

---
+++ src/components/TheAvatar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="avatar">
+        <img :src="image || DefaultAvatar">
+    </div>
+</template>
+
+<script setup>
+import DefaultAvatar from '../assets/avatarDefault.png'
+defineProps({
+    image: String,
+    widthString: {
+        type: String,
+        default: "33px"
+    },
+    heightString: {
+        type: String,
+        default: "33px"
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+img {
+    width: v-bind(widthString);
+    height: v-bind(heightString);
+    border-radius: 50%;
+    overflow: hidden;
+}
+</style>
