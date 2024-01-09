78406c5f5727e168ba18cde838bc4021cb3e7cda
LiangHeng <lianghengcn@gmail.com>
2024-01-09 21:04:44 +0800

    chroe: change the timestamp

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,4 @@
-import { formatDistanceToNowStrict, parseISO } from "date-fns";
+import { formatDistanceToNowStrict, parseISO, differenceInDays, format } from "date-fns";
 import { zhCN } from 'date-fns/locale'
 /**
  * @description cast ISO8061 timestamp to human readble timeformat,  
@@ -8,9 +8,18 @@ import { zhCN } from 'date-fns/locale'
  * @returns {String}
  */
 export function formatTimestamp(timestamp) {
+    const dateNow = new Date;
     const date = parseISO(timestamp);
     const distance = formatDistanceToNowStrict(date, { locale: zhCN, addSuffix: true });
+    const distanceInDays = differenceInDays(dateNow, date);
+    if (distanceInDays >= 3) {
+        return formatDate(date);
+    }
     return distance;
 }
-
-export default { formatTimestamp };
+export function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const formattedDate = format(date, "yyyy年M月d日HH时mm分", { locale: zhCN });
+    return formattedDate;
+}
+export default { formatTimestamp, formatDate };
