3a832f2349751fc383da4870a644574533d800f3
LiangHeng <lianghengcn@gmail.com>
2024-03-03 10:18:17 +0800

    fix: some preparation for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/picnia/',
-  server: {
-    // host: '192.168.2.110',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(async ({ mode }) => {
+  const isDev = mode === 'development'
+  const env = await loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    base: '/picnia/',
+    server: {
+      // host: '192.168.2.110',
+      proxy: {
+        '/api': {
+          target: isDev ? 'http://localhost:3000' : `http://${env.VITE_SERVER_IP}:${env.VITE_PORT}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        }
       }
     }
   }
